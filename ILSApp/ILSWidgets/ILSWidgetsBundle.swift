ef83ac0ba32e07e009132367530720ee2382b953
VQA Developer <vqa@developer.com>
2026-03-01 13:17:22 -0500

    auto-claude: subtask-1-1 - Create ILSWidgets folder structure: ILSApp/ILSWidgets/ILSWidgetsBundle.swift and ILSApp/ILSWidgets/ILSWidgets.entitlements

---
+++ ILSApp/ILSWidgets/ILSWidgetsBundle.swift
@@ -0,0 +1,11 @@
+import WidgetKit
+import SwiftUI
+
+@main
+@available(iOS 17.0, *)
+struct ILSWidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        SessionWidget()
+        ServerStatusWidget()
+    }
+}
